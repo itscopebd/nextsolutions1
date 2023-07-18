af7b1219a1523b7fff0a231d56b2b3570f388265
itscopebd <itscopebd@gmail.com>
2023-07-18 22:42:48 +0600

    About our product section added

---
+++ src/components/AboutOurProduct/AboutOurProduct.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AboutOurProduct = () => {
+    return (
+        <div className='my-20'>
+            <h3 className='text-3xl font-bold text-center text-capitalize'>About Our Product</h3>
+            <div className='w-6/12 text-center mx-auto mt-6'>
+                <p>
+                    From the writings of one of the founding fathers, Benjamin Franklin, to people of The Greatest Generation, being thrifty is as American as apple pie.
+                    You don't have to pay a lot to receive quality items such as vintage bracelets, luxury jewelry sets, and more, if you know how to discern fine from mediocre. We do this for you!  Our quality items - priced right - show off your savvy and vibe.
+                    </p>
+                    <button className='btn btn-primary mt-5 '>See More</button>
+            </div>
+        </div>
+    );
+};
+
+export default AboutOurProduct;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../components/NavBar/NavBar';
 import Products from '../components/Products/Products';
+import AboutOurProduct from '../components/AboutOurProduct/AboutOurProduct';
 
 const Home = () => {
     return (
         <div>
             <NavBar/>
             <Products></Products>
+            <AboutOurProduct></AboutOurProduct>
         </div>
     );
 };
